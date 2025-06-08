0b9d6be8e84ed6e6d02c865709a900fc9836517a
Stanley Pang <stanley.pang@mail.utoronto.ca>
2025-06-08 03:24:25 -0400

    icon sizing fix

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Icon = ({ link, logo: Logo, size }) => {
+  const sizeMap = {
+    4: 'size-4',
+    5: 'size-5', 
+    6: 'size-6',
+    8: 'size-8',
+    10: 'size-10',
+    12: 'size-12'
+  }
+  
+  const logoSize = sizeMap[size];
+
   return (
-		<a href={link} target="_blank" rel="noopener noreferrer">
-			<Logo className={`size-${size} hover:scale-120 transition-transform duration-300`} />
-		</a>
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <Logo className={`${logoSize} hover:scale-110 transition-transform duration-300`} />
+    </a>
   )
 }
 
